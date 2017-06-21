cfb84d016cd650485e4d8327130a6004bb92d4fd
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-20 21:26:12 -0700

    Update 014. Remove Target Char.py

--- 014. Remove Target Char.py
+++ 014. Remove Target Char.py
@@ -12,30 +12,20 @@ Traverse the string and put the non-target character into the char array one by 
 
 '''
 
-def removeChar(s, target):
+
+def moveChar(s, target):
     s = list(s)
-    slow = fast = 0
-    while fast < len(s):
-        # skip first several valid chars
-        while slow < len(s) and s[slow] != target:
-            slow += 1
-
-        fast = slow
-        while fast < len(s) and s[fast] == target:
-            fast += 1
-
-        while fast < len(s) and s[fast] != target:
-            s[slow] = s[fast]
-            s[fast] = target    # set fast to target 
-            slow += 1
-            fast += 1
+    j = 0
+    for i in range(len(s)):
+        if s[i] != target:
+            s[j], s[i] = s[i], s[j]
+            j += 1
+    return ''.join(s)
 
 
-    return ''.join(s[:slow])
-
-s = 'abbcdbbafsjo'
+s = 'abbcdbbbbbbb'
 target = 'b'
-print removeChar(s, target)
+print moveChar(s, target)
 
 
 
