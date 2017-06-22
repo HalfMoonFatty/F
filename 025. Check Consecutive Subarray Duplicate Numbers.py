b4263bf73adb15ce95a5aa226554aab15dbbf41e
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-21 20:23:47 -0700

    Create 025. Check Consecutive Subarray Duplicate Numbers.py

---
+++ 025. Check Consecutive Subarray Duplicate Numbers.py
@@ -0,0 +1,24 @@
+'''
+Problem:
+
+给一个array, 然后给 个k, 让你check连续的k个integer是否含有dulplicate。
+
+很简单的， 窗口为K的hashset一直扫一遍就行了。  
+'''
+
+def containsNearbyDuplicate(nums, k):
+    i = j = 0
+    window = set()
+    while j < len(nums):
+        if j - i + 1 > k:  # window size = j - i + 1
+            window.remove(nums[i])
+            i += 1
+        if nums[j] in window:
+            return True
+        window.add(nums[j])
+        j += 1
+    return False
+
+
+nums = [1,2,3,2,1,4]
+print containsNearbyDuplicate(nums, 3)
