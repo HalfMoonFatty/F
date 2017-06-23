a14f02df4d7a488773c6ed6bdca3de241a028b75
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-22 21:42:29 -0700

    Update and rename 032. Binary Tree longest PATH.py to 032. Binary Tree longest Path.py

--- 032. Binary Tree longest Path.py
+++ 032. Binary Tree longest Path.py
@@ -12,8 +12,8 @@ def findLongestPath(root):
         left = getLongestPath(root.left)
         right = getLongestPath(root.right)
         across = left[0] + right[0] + 1
-        longestPath = max(across, max(left[1], right[1]))
-        depth = max(left[0], right[0])+1
+        longestPath = max(across, max(left[1], right[1]))    # longestPath = max (across, left.longestPath, right.longestPath)
+        depth = max(left[0], right[0])+1     # depth = max(left.depth, right.depth)
         return (depth, longestPath)
 
     if not root: return 0
