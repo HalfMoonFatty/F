15896e08618669de19de0fcc4ff89515e4583134
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-22 21:09:24 -0700

    Update 016. Implement strStr().py

--- 016. Implement strStr().py
+++ 016. Implement strStr().py
@@ -27,3 +27,35 @@ class Solution(object):
             if j == n:
                 return i
         return -1
+
+    
+    
+'''
+Follow-up: Strstr 2d Array
+
+把第一个haystack参数变成一个二维数组，然后找needle在haystack第一次出现的位置，同样的不能用string相关方法.
+还是返回把haystack[][]二维数组看成一维以后的第一次匹配的字符下标，
+Example: haystack := [[a,b,c,c], [c,b,d]], needle := [c,c,b,d] , return 2; needle := [b, d] return 5 (5是把haystack看成一维以后的找到第一个bd以后b的下标)
+'''
+
+
+def strStr2D(haystack, needle):
+    total = 0
+    for row in range(len(haystack)):
+        for col in range(len(haystack[0])):
+            i, j, k = row, col, 0
+            while i < len(haystack) and j < len(haystack[0]) and k < len(needle) \
+            and haystack[i][j] == needle[k]:
+                k += 1
+                j += 1
+                if j == len(haystack[0]):
+                    i += 1
+                    j = 0
+                    
+            if k == len(needle):
+                return total + j - 1
+
+        total += len(haystack[0])
+
+    return -1
+
