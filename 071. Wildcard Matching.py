7c652d33d5b17437ecca1528576bc5a0b1778783
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-26 18:29:39 -0700

    Create 071. Wildcard Matching.py

---
+++ 071. Wildcard Matching.py
@@ -0,0 +1,8 @@
+'''
+Problem:
+
+Given a list of words, find whether a given target word exists. Should support “.” which matches any character. 
+
+Follow up: support “*” which matchs 0 or more characters.
+
+'''
