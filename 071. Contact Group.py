37daed66346be672652953dd453f441b8ddc6722
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-27 11:47:46 -0700

    Update 071. Contact Group.py

--- 071. Contact Group.py
+++ 071. Contact Group.py
@@ -15,3 +15,52 @@ Combine lists with identical emails, and output tuples:
 (list 3) (e@e.com)
 
 '''
+
+
+import collections
+
+def contactGroup(contact):
+
+    def findRoot(unionSet, root):
+        if root == unionSet[root]:
+            return root
+        unionSet[root] = findRoot(unionSet, unionSet[root])
+        return unionSet[root]
+
+
+    def union(unionSet, root1, root2):
+        root1 = findRoot(unionSet, unionSet[root1])
+        root2 = findRoot(unionSet, unionSet[root2])
+        unionSet[root1] = root2
+        return
+
+
+    emailToPerson = {}
+    unionSet = {}    # as need to group by keys
+
+    for person in contact.keys():
+        unionSet[person] = person
+        curRoot = person
+
+        for email in contact[person]:
+            if not emailToPerson.has_key(email):
+                emailToPerson[email] = person
+                continue
+            newRoot = findRoot(unionSet, emailToPerson[email])
+
+            if newRoot != curRoot:
+                union(unionSet, newRoot, curRoot)
+                curRoot = newRoot
+
+
+    group = collections.defaultdict(list)
+    for person in unionSet.keys():
+        root = findRoot(unionSet,person)
+        group[root].append(person)
+    print group
+    return group.values()
+
+contact = {1:["a@a.com", "b@b.com"], 2:["b@b.com", "c@c.com"], 3:["e@e.com"], 4:["a@a.com"]}
+print contactGroup(contact)
+
+
