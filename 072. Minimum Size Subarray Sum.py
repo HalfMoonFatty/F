f70a8c9fb7502894722a4fe24b2d4ec33430ac35
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-26 18:48:39 -0700

    Update 072. Minimum Size Subarray Sum.py

--- 072. Minimum Size Subarray Sum.py
+++ 072. Minimum Size Subarray Sum.py
@@ -35,3 +35,90 @@ class Solution:
                 minSize = min(minSize, j-i+1)    # update minSize
 
         return minSize if minSize <= len(nums) else -1
+
+    
+
+    
+    
+# Follow up: Extend to 2D and find if rectangle sum = k
+# Similar Problem: 363. Max Sum of Rectangle No Larger Than K.py
+
+import sys
+def sumSubmatrixEqualsK(matrix, k):
+
+
+    if not matrix: return 0
+
+    res = -sys.maxint-1
+    for left in range(0,len(matrix[0])):
+        rowSum = [0] * len(matrix)
+        for right in range(left, len(matrix[0])):
+            sums = {0}
+            curSum = 0
+            # accumulate current column to rowSum
+            for i in range(len(matrix)):
+                rowSum[i] += matrix[i][right]
+                curSum += rowSum[i]
+                if curSum - k in sums:
+                    return True
+                sums.add(curSum)
+
+    return False
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+k = 27
+print sumSubmatrixEqualsK(matrix, k)
+
+
+
+
+'''
+Problem:
+Given a non-empty 2D matrix matrix and an integer k, find the max sum of a rectangle in the matrix such that its sum is no larger than k.
+Example:
+Given matrix = [
+[1,  0, 1],
+[0, -2, 3]
+]
+k = 2
+The answer is 2. Because the sum of rectangle [[0, 1], [-2, 3]] is 2 and 2 is the max number no larger than k (k = 2).
+Note:
+The rectangle inside the matrix must have an area > 0.
+What if the number of rows is much larger than the number of columns?
+'''
+
+class Solution(object):
+    def maxSumSubmatrix(self, matrix, k):
+        """
+            :type matrix: List[List[int]]
+            :type k: int
+            :rtype: int
+            """
+
+        if not matrix: return 0
+
+        res = -sys.maxint-1
+        for left in range(0,len(matrix[0])):
+            tmp = [0] * len(matrix)
+            for right in range(left, len(matrix[0])):
+
+                # accumulate current column to tmp
+                for i in range(len(matrix)):
+                    tmp[i] += matrix[i][right]
+
+                # find the max subarray no more than K
+                accuSum = [0]
+                curSum,curMax = 0, -sys.maxint -1
+                for v in tmp:
+                    curSum += v
+                    ind = bisect.bisect_left(accuSum,curSum-k)
+                    if ind < len(accuSum):
+                        curMax = max(curMax,curSum-accuSum[ind])
+                    # early return
+                    #if curMax == k: return k
+                    bisect.insort(accuSum,curSum)
+
+                res = max(res, curMax)
+
+        return res
