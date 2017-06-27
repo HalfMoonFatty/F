d9701343b5f7036f6e9dc63b24fe104678506eb2
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-26 22:22:12 -0700

    Update 053. Add and Search Word - Data structure design.py

--- 053. Add and Search Word - Data structure design.py
+++ 053. Add and Search Word - Data structure design.py
@@ -28,6 +28,16 @@ Application:
 # Space complexity: O(number of words * averageLen(word))'
 
 
+'''
+Problem:
+
+Given a list of words, find whether a given target word exists. Should support “.” which matches any character. 
+
+Follow up: support “*” which matchs 0 or more characters.
+
+'''
+
+
 class TrieNode():
     def __init__(self):
         self.children = {}
@@ -57,7 +67,7 @@ class WordDictionary(object):
         for i in range(len(word)):
         
             # regular char: exact matching
-            if len(node.children)>0 and word[i] != '.':
+            if len(node.children)>0 and word[i] != '.' and word[i] != '*':
                 if not node.children.has_key(word[i]):
                     return False
                 node = node.children[word[i]]
@@ -69,9 +79,25 @@ class WordDictionary(object):
                     node = tmp.children[k] # re-assign value to node
                     if self.searchHelper(word[i+1:], node):
                         return True
+
+            elif len(node.children)>0 and word[i] == '*':
+                if self.searchHelper(word[i+1:], node):   # "*" match empty string
+                    return True
+                tmp = node   
+                for k in node.children.keys():
+                    node = tmp.children[k] # re-assign value to node
+                    if self.searchHelper(word[i:], node):    # word index keeps the same but advance trie node
+                        return True
+
                         
             # still have more letters to search (as we are in the loop), but no more children to search.
             else:
                 return False
 
         return node.isWord
+
+
+wd = WordDictionary()
+
+wd.addWord("badddd")
+print wd.search("bade*") 
