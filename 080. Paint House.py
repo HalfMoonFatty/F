f1c7ac51a5fe5507e51ea24bdc9fdacb54374781
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-28 11:06:10 -0700

    Create 080. Paint House.py

---
+++ 080. Paint House.py
@@ -0,0 +1,8 @@
+'''
+Problem:
+
+n houses, k colors. neighboring houses cannot be painted with the same color.
+
+NOTE: neighboring relationship is given by adjacent list which means a house may have multiple neighbors.
+'''
+
