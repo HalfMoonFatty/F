871a99051a44656bcca7575fe8c0427f4d15be4c
HalfMoonFatty <allofotherstuff@gmail.com>
2017-06-29 14:29:38 -0700

    Create 087. Find Max Overlap Time.py

---
+++ 087. Find Max Overlap Time.py
@@ -0,0 +1,45 @@
+'''
+Problem:
+
+Given a series Intervals [startTime, stopTime). Find a timestamps that appear most times in the interval.
+
+e.g. [1,3), [2, 7), [4, 8), [5, 9)
+5, 6 appears 3 times, so return 5,6.
+
+'''
+
+
+def findMaxOverLapTime(intervals):
+    if not intervals: return []
+
+    points = []
+    for interval in intervals:
+        points.append([interval[0], 's'])
+        points.append([interval[1], 'e'])
+    points.sort()
+
+    maxInterval = 0
+    nIntervals = 0
+    start = end = 0
+
+    for p in points:
+        if p[1] == 's':
+            nIntervals += 1
+            if nIntervals > maxInterval:
+                maxInterval = nIntervals
+                start = p[0]
+                #end = p[0]
+        else:
+            if nIntervals == maxInterval:
+                end = p[0]
+
+            nIntervals -= 1
+        print p, start, end
+    result = []
+    for i in range(start, end):
+        result.append(i)
+    return result
+
+
+intervals = [[1,3],[2,7],[4,8],[5,9]]
+print findMaxOverLapTime(intervals)
