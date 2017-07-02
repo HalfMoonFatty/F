056da3660d10229b065dfc341a3e39644ff5e982
HalfMoonFatty <allofotherstuff@gmail.com>
2017-07-01 22:32:33 -0700

    Create 096. Find Names in Post.py

---
+++ 096. Find Names in Post.py
@@ -0,0 +1,13 @@
+'''
+Problem:
+
+在一个post 里面找出所有的name分别出现的位置，post 会很长
+
+Follow-up: names 如果很多怎么办？
+'''
+
+'''
+Follow-up: Use TrieTree,
+word.length = k, post.length = n
+Time complexity: O(nk), space complexity: total number of the node in trie tree
+'''
