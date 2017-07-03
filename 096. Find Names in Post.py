3c66e5026bc187eb5ce29194fbbf4448d37f1571
HalfMoonFatty <allofotherstuff@gmail.com>
2017-07-03 14:13:02 -0700

    Update 096. Find Names in Post.py

--- 096. Find Names in Post.py
+++ 096. Find Names in Post.py
@@ -11,3 +11,66 @@ Follow-up: Use TrieTree,
 word.length = k, post.length = n
 Time complexity: O(nk), space complexity: total number of the node in trie tree
 '''
+
+class TrieNode(object):
+    def __init__(self):
+        self.children = dict()
+        self.isWord = False
+
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def insert(self, word):
+        node = self.root
+        for letter in word:
+            if not node.children.has_key(letter):
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
+        node.isWord = True
+        return
+
+            
+    def search(self, post, index):
+
+        result = []
+        word = ''
+        node = self.root
+
+        # index 和 node 都往前走
+        # 走到 "not node.children.has_key(letter)"
+        while index < len(post):
+            if node.isWord:
+                result.append(word)
+            letter = post[index]
+            if not node.children.has_key(letter):
+                return result
+            node = node.children[letter]
+            word += letter
+            index += 1
+
+        if node.isWord:
+            result.append(word)
+        return result
+        
+
+
+    def findWords(self, words, post):
+        if not words: return []
+        
+        result = collections.defaultdict(list)
+        # add all words to the trie
+        for word in words:
+            self.insert(word)
+        # for each index in post, search if any word starts from index i
+        for i in range(len(post)):
+            res = search(post, i)
+
+        for word in res:
+            result[word].append(i)
+
+        return result
+
