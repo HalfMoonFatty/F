53422189324b0e10688b56e725d8c6d89b10a9c3
HalfMoonFatty <allofotherstuff@gmail.com>
2017-07-09 21:36:13 -0700

    Update 094. Is Subtree.py

--- 094. Is Subtree.py
+++ 094. Is Subtree.py
@@ -7,29 +7,31 @@ Check if a tree is a subtree of another tree.
 
 # Time complexity: O(nk) -k nodes in subtree
 
-def isValid(root1, root2):
 
-    def helper(root1, root2):
-        if not root1 and not root2:
-            return True
-        if (not root1 and root2) or (root1 and not root2):
+class Solution(object):
+    def isSubtree(self, s, t):
+
+        def isValid(root1, root2):
+            if not root1 and not root2:
+                return True
+            if (not root1 and root2) or (root1 and not root2):
+                return False
+            if root1.val == root2.val:
+                return isValid(root1.right, root2.right) and isValid(root1.left, root2.left)
             return False
-        if root1.val == root2.val:
-            return helper(root1.right, root2.right) && helper(root1.left, root2.left)
-        return False
-
+            
+            
+            
+        if not s and not t:
+            return True
         
-
-    if not root1 and not root2:
-        return True
-
-    result = False
-    if root1 and root2:
-        if root1.val == root2.val:
-            result = helper(root1, root2)
-        if not result:
-            result = isValid(root1.right, root2)
-        if not result:
-            result = isValid(root1.left, root2)
-    return result
-
+        result = False
+        if s and t:
+            if s.val == t.val:
+                result = isValid(s, t)
+            if not result:
+                result = self.isSubtree(s.left, t)
+            if not result:
+                result = self.isSubtree(s.right, t)
+        return result
+                
