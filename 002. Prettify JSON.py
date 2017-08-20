42427b0e4661b8a8b73e3f93f55b6317bb41574f
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-19 20:58:25 -0700

    Update 002. Prettify JSON.py

--- 002. Prettify JSON.py
+++ 002. Prettify JSON.py
@@ -37,14 +37,17 @@ def prettyJson(s):
 
   for i in range(len(s)):
     c = s[i]
+
     if c == ' ':
       continue
+
     elif c in ('{', '['):
       indent += TAB_SIZE
       maybe_prefix = buffer + '\n' if buffer != len(buffer) * ' ' else ''
       pretty += (maybe_prefix + indent * ' ' + c + '\n')
       indent += TAB_SIZE
       buffer = indent * ' '
+
     elif c in ('}', ']'):
       indent -= TAB_SIZE
       maybe_prefix = buffer + '\n' if buffer != len(buffer) * ' ' else ''
@@ -52,17 +55,21 @@ def prettyJson(s):
       pretty += (maybe_prefix + indent * ' ' + c + maybe_comma + '\n')
       indent -= TAB_SIZE
       buffer = indent * ' '
+	
     elif c == ',':
       if s[i - 1] in ('}', ']'):
         continue  # Already handled by elif block above.
       pretty += (buffer + c + '\n')
       buffer = indent * ' '
+	
     elif c == ':':
       buffer += (c + ' ')
+
     else:
       buffer += c
 
   return pretty[2:]  # Skip first 2 chars, which are always newlines.
+
 
 s = """
 {"id": "0001", "type":      "donut","name": "Cake","ppu": 0.55, "batters":{"batter":[{ "id": "1001", "type": "Regular" },{ "id": "1002", "type": "Chocolate" }]},"topping":[{ "id": "5001", "type": "None" },{ "id": "5002", "type": "Glazed" }]}"
