2c27afb0094dd41fc10680016251946fa7800e30
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-27 22:54:31 -0700

    Update 056. Permutations_Derangement.py

--- 056. Permutations_Derangement.py
+++ 056. Permutations_Derangement.py
@@ -62,12 +62,33 @@ class Solution(object):
         permuteUniqueHelper(nums, 0, Set(), result)
         return result
     
+
     
+    
+'''
+Problem 3:
+
+A derangement is a permutation of the elements of a set, such that no element appears in its original position.
+
+There's originally an array consisting of n integers from 1 to n in ascending order, you need to find the number of derangement it can generate.
+
+Also, since the answer may be very large, you should return the output mod 109 + 7.
+
+'''
+    
+    
+class Solution(object):
+    def findDerangement(self, n):
+
+        dp = [0, 1]
+        for x in range(2, n + 1):
+            dp.append(x * (dp[- 1] + dp[-2]) % (10**9 + 7))
+        return dp[n - 1]
     
     
     
 '''
-Problem 3: 
+Problem 4: 
 
 A "derangement" of a sequence is a permutation where no element appears in its original position. 
 For example ECABD is a derangement of ABCDE, given a string, may contain duplicate char, please out put all the derangement 
