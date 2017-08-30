57bfec8f0eb8f1a15609cec6e3021f55155c5a03
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-29 18:17:02 -0700

    Update 017. Minimum Depth of Binary Tree.py

--- 017. Minimum Depth of Binary Tree.py
+++ 017. Minimum Depth of Binary Tree.py
@@ -38,6 +38,7 @@ def minDepth(root):
             cur = q.popleft()
             if not cur.left and not cur.right:
                 result = level
+                break
             if cur.left: q.append(cur.left)
             if cur.right: q.append(cur.right)
     return result
