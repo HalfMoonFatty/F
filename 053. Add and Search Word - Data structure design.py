22115ca83568d9baf4df2b6b7df4454f784aadf7
HalfMoonFatty <allofotherstuff@gmail.com>
2017-08-31 19:52:02 -0700

    Update 053. Add and Search Word - Data structure design.py

--- 053. Add and Search Word - Data structure design.py
+++ 053. Add and Search Word - Data structure design.py
@@ -67,20 +67,21 @@ class WordDictionary(object):
         for i in range(len(word)):
         
             # regular char: exact matching
-            if len(node.children)>0 and word[i] != '.' and word[i] != '*':
+            if len(node.children) > 0 and word[i] != '.' and word[i] != '*':
                 if not node.children.has_key(word[i]):
                     return False
                 node = node.children[word[i]]
                 
             # wild char"."
-            elif len(node.children)>0 and word[i] == '.':
+            elif len(node.children) > 0 and word[i] == '.':
                 tmp = node    # must remember the current iterator node, as other recursion path may modify node
                 for k in node.children.keys():
                     node = tmp.children[k] # re-assign value to node
                     if self.searchHelper(word[i+1:], node):
                         return True
 
-            elif len(node.children)>0 and word[i] == '*':
+            # wild char"*"
+            elif len(node.children) > 0 and word[i] == '*':
                 if self.searchHelper(word[i+1:], node):   # "*" match multiple chars
                     return True
                 tmp = node   
@@ -89,7 +90,6 @@ class WordDictionary(object):
                     if self.searchHelper(word[i:], node):    # "*" match 0 char
                         return True
 
-                        
             # still have more letters to search (as we are in the loop), but no more children to search.
             else:
                 return False
@@ -98,6 +98,5 @@ class WordDictionary(object):
 
 
 wd = WordDictionary()
-
 wd.addWord("badddd")
 print wd.search("bade*") 
