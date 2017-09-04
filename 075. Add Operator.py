4e716c71da2cd7a3d77f21da954eabef3c764656
HalfMoonFatty <allofotherstuff@gmail.com>
2017-09-03 21:38:29 -0700

    Update 075. Add Operator.py

--- 075. Add Operator.py
+++ 075. Add Operator.py
@@ -35,3 +35,31 @@ def addOperator(number):
     return list(result)
 
 print addOperator('103')  
+
+
+
+
+'''
+Problem:
+
+给你一个数字由1-9组成，给你一个target value，可以在数字中间加 + 或者 -, 问你有多少种组合可以得到target value。
+同时有一个eval method，把表达式传进去，返回计算结果。
+
+写完后，让跑test,最后问时间复杂度。
+'''
+
+
+
+import collections
+def findTargetSumWays(nums, S):
+
+    dp = [collections.defaultdict(int) for _ in range(len(nums)+1)]
+    dp[0][0] = 1
+    
+    for i in range(1,len(nums)+1):
+        for j in range(1,i+1):
+            for k in dp[i-j].keys():
+                dp[i][k+nums[i-j]] += dp[i-j][k]
+                dp[i][k-nums[i-j]] += dp[i-j][k]
+
+    return dp[-1][S]
