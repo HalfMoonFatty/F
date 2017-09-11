74e973b6c232db7f206ca689fe75639e5d3345ee
HalfMoonFatty <allofotherstuff@gmail.com>
2017-09-11 11:33:02 -0700

    Update 106. Decode and Encode String.py

--- 106. Decode and Encode String.py
+++ 106. Decode and Encode String.py
@@ -1,5 +1,5 @@
 '''
-Problem:
+Problem: Decode String
 
 Given an encoded string, return it's decoded string.
 The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
@@ -44,8 +44,8 @@ class Solution(object):
     
     
 '''
-Problem:
-
+Problem: Encode String
+ 
 Given a non-empty string, encode the string such that its encoded length is the shortest.
 The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times.
 
