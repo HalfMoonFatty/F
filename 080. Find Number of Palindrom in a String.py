345f7d8da255b10686ed24b04f2cd24442a2c595
HalfMoonFatty <allofotherstuff@gmail.com>
2017-09-10 18:07:26 -0700

    Update 080. Find Number of Palindrom in a String.py

--- 080. Find Number of Palindrom in a String.py
+++ 080. Find Number of Palindrom in a String.py
@@ -1,5 +1,5 @@
 '''
-Problem 1:
+Problem 1: Number of Palindrom Substring
 
 Given a string, the task is to count all palindrome substring in a given string. 
 
@@ -35,6 +35,35 @@ def countPS(s):
 
 print countPS("abaab")
 print countPS("abbaeae")
+
+
+
+
+'''
+Problem 2: Number of Palindrom Subsequence
+'''
+
+def countPS(s):
+
+    n = len(s)
+    isPal = [[False] * (n+1) for _ in range(n+1)]
+    dp = [[0]*(n+1) for _ in range(n+1)]
+
+    for i in range(1,n+1):
+        for j in range(i-1,-1,-1):
+            if s[j-1] == s[i-1]:
+                dp[j][i] = dp[j][i-1] + dp[j+1][i] + 1
+            else:
+                dp[j][i] = dp[j][i-1] + dp[j+1][i] - dp[j+1][i-1]
+    return dp[0][n-1]
+
+
+
+print countPS("abaab")
+print countPS("abbaeae")
+
+
+
 
 
 
